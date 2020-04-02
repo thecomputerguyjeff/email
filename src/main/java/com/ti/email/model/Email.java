9f809032855560a0a89ea54af0c321072636d3f9
Chumi Goldwag <chumigoldwag@gmail.com>
2020-04-01 20:58:47 -0500

    updated work

---
+++ src/main/java/com/ti/email/model/Email.java
@@ -0,0 +1,9 @@
+package com.ti.email.model;
+
+public class Email {
+    public int EmailId;
+    public String sendToEmailAddress;
+    public String sentFromEmailAddress;
+    public String subject;
+    public String emailText;
+}
