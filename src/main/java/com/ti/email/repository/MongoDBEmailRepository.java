19bc230b8efbd18f7cecf90d6ae94a4a36ad21d2
Malka Bachrach <malka.bachrach@ti.htc.edu>
2020-04-27 16:40:16 -0500

    added my changes

--- src/main/java/com/ti/email/repository/MongoDBEmailRepository.java
+++ src/main/java/com/ti/email/repository/MongoDBEmailRepository.java
@@ -31,10 +31,6 @@ public class MongoDBEmailRepository implements EmailRepository{
         return operations.findOne(query,Email.class);
     }
 
-    public List<Email> findAllSentByBy_id(String userId) {
-        Query query=query(where("_id").is(userId));
-        query.fields().exclude("_id");
-        return operations.find(query,Email.class);
-    }
+
 }
 
