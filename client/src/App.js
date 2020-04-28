4c284dba06ce2c6fe49edfc7a3e702abf938b81b
Jeff Mayer <thecomputerguy.jeff@gmail.com>
2020-04-27 21:18:39 -0500

    add text

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        This is react App.js
         {/* <LoginScreen setUserId={(userId) => {this.setState(userId)}} /> */}
         {/* <Inbox userId ={this.state.userId}/>>  */}
       
