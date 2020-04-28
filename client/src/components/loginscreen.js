3ea27ef87eeb6b2b98acaf73d9a480578c026527
Chumi Goldwag <chumigoldwag@gmail.com>
2020-04-27 20:30:54 -0500

    login screen

--- client/src/components/loginscreen.js
+++ client/src/components/loginscreen.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class LoginScreen extends React.Component {
-
+    const LoginScreen = (props) =>
+    return(
+<Form>
+<FormGroup>
+        <Label for="exampleEmail">User Name</Label>
+        <Input type="username" name="username" id="exampleName" placeholder="John Doe" />
+        </FormGroup>
+        <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="password goes here" />
+        </FormGroup>
+        <Button>Submit</Button>
+</Form>
+);
 }
 
 export default LoginScreen
