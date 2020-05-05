671ab4e1d23c94b920e0eba6c41a2746d7f0461d
eobodyanik <eobodyanik@getnerdio.com>
2020-05-04 19:33:00 -0500

    Removed logo

--- client/src/components/inbox.js
+++ client/src/components/inbox.js
@@ -5,10 +5,12 @@ class Inbox extends React.Component {
     render() {
         return (
           <div>
+            {
             <div id="loader">
                 <i class="fa fa-refresh fa-spin fa-5x fa-fw"></i>
                 Loading&nbsp;inbox&nbsp;in&nbsp;progress.&nbsp;Please&nbsp;wait...
             </div>
+            }
           </div>
         );
       }}
