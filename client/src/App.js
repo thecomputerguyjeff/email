368c47bffe4d5444a1eb41d7501e2942188acd83
Chumi Goldwag <chumigoldwag@gmail.com>
2020-05-04 22:39:01 -0500

    login screen finished!

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
-// import LoginScreen from './components/loginscreen.js'
-import Inbox from './components/inbox.js'
-// import { Button } from 'reactstrap';
+import LoginScreen from './components/loginscreen.js'
+import { Button } from 'reactstrap';
 class App extends React.Component {
 
 
   constructor(props) {
     super(props)
     this.state = {
-      userId : "5e795552096fba1f7837dd01",
+      userId : null,
+
     }
   }
 
   render() {
     return (
-      <div>        
-        {/* { <LoginScreen setUserId={(userId) => {this.props(userId)}} /> } */}
-        {this.state.userId&&<Inbox userId ={this.state.userId}/>}
+      <div>
+        {/*//This is react App.js*/}
+        { <LoginScreen setUserId={(userId) => {this.props(userId)}} /> }
+        {/* <Inbox userId ={this.state.userId}/>>  */}
       
 
       </div>
--- client/src/components/loginscreen.js
+++ client/src/components/loginscreen.js
@@ -7,81 +7,27 @@ import {
 import '../App.css';
 
 class LoginScreen extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "",
-            password: ""
-        };
-
-        this.submit = this.submit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange({ target }) {
-        this.setState({
-            [target.name]: target.value
-        });
-    }
-    submit() {
-        debugger;
-        let data = {
-            'username': this.state.username,
-            'password': this.state.password
-        }
-        fetch('https://ti-email.herokuapp.com/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-            .then((response) =>{
-                if(response.status===200){
-                    this.setState({renderModal:true})
-                }
-            })
-            .catch((error)=> {
-                console.log("error=",error);
-            });
-
-    }
     render() {
         return(
-            <div className ="box flex">
-            {/*<Container className="box flex">*/}
+            <div class ="box flex">
+            <Container className="box flex">
     <Form>
         <Col>
-        <FormGroup >
+    <FormGroup >
         <h2>Sign In</h2>
         <Label for="exampleEmail">User Name</Label>
-        <Input type="username"
-               name="username"
-               id="exampleName"
-               value={ this.state.username }
-               onChange={ this.handleChange }
-               placeholder="John Doe" />
+        <Input type="username" name="username" id="exampleName" placeholder="John Doe" />
         </FormGroup>
         <FormGroup>
         <Label for="examplePassword">Password</Label>
-        <Input type="password"
-               name="password"
-               id="examplePassword"
-               value={ this.state.password }
-               onChange={ this.handleChange }
-               placeholder="Password goes here" />
+        <Input type="password" name="password" id="examplePassword" placeholder="password goes here" />
         </FormGroup>
         </Col>
-        <button onClick={ this.submit }>Submit</button>
-        <script>
-
-        </script>
+        <Button>Submit</Button>
 </Form>
-            {/*</Container>*/}
-            </div>
+            </Container></div>
 
-);}
-//fetch statement....dif btwn post, get
-
+);
+}
 }
 export default LoginScreen
