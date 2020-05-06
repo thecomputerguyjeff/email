11cc98390a9334d142e6482e1dad6fa18bb920c7
Chumi Goldwag <chumigoldwag@gmail.com>
2020-05-06 16:39:58 -0500

    submit button, doesnt work yet

--- client/src/components/loginscreen.js
+++ client/src/components/loginscreen.js
@@ -13,21 +13,33 @@ class LoginScreen extends React.Component {
             <Container className="box flex">
     <Form>
         <Col>
-    <FormGroup >
+        <FormGroup >
         <h2>Sign In</h2>
         <Label for="exampleEmail">User Name</Label>
-        <Input type="username" name="username" id="exampleName" placeholder="John Doe" />
+        <Input type="username" onChange={submitEmail()} name="username" id="exampleName" placeholder="John Doe" />
         </FormGroup>
         <FormGroup>
         <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password goes here" />
+        <Input type="password" onChange={submitForm()} name="password" id="examplePassword" placeholder="Password goes here" />
         </FormGroup>
         </Col>
-        <Button>Submit</Button>
+        <button onClick="submitFunction()">Submit</button>
+        <script>
+
+        </script>
 </Form>
             </Container></div>
 
-);
-}
+);}
+    submitForm(e) {
+        e.preventDefault();
+        console.log(`Email: ${ this.state.email }`)
+    }
+
+        function submitFunction() {
+            //loginService.login(credentials);
+            document.getElementById("userId");
+        }
+
 }
 export default LoginScreen
